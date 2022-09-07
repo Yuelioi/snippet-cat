d59cb4f5a91a4c8af41792ef09cd9e3ad17b3288
yueloi <yuelioi1210@gmail.com>
2022-09-08 03:10:33 +0800

    已完成webdav同步功能

--- 1.js
+++ 1.js
@@ -1,43 +1,116 @@
 
-async function* handleSnippets(...args) {
+// async function* handleSnippets(...args) {
 
-  console.log(args); // ① 初始化的参数 1,2,3
+//   console.log(args); // ① 初始化的参数 1,2,3
+//   let value1 = yield args;
+//   console.log(value1); // ③ { value: [ 1, 2, 3 ], done: false }
+//   console.log(args); // ④ 初始化的参数 1,2,3
+//   let value2 = yield 2;
+//   console.log(value2);
+//   let value3 = yield 3;
+//   console.log(value3);
+// }
 
-  let value1 = yield args;
+// function addGroup() {
+//   let iter = handleSnippets(1,2,3);  //  打印
+//   let t1 = iter.next(); // 
+//   console.log(t1); // ② Promise { <pending> }
 
 
-  console.log(value1); // ③ { value: [ 1, 2, 3 ], done: false }
+//   t1.then(data=>{
+//     iter.next(data);iter.next(1);
+//   }); // ③
+// }
 
-  console.log(args); // ④ 初始化的参数 1,2,3
 
-  let value2 = yield 2;
-  console.log(value2);
+import { AuthType, createClient } from "webdav";
 
-  let value3 = yield 3;
-  console.log(value3);
+import fs from "fs";
+import path from "path";
+
+
+const client = createClient(
+
+  "https://drive.yuelili.com/dav",
+
+  {
+    username: "435826135@qq.com",
+    password: "VqhY6VQNGLAg8tYitfebxrI02srnqrWr"
+  }
+);
+
+
+
+if (await client.exists("/Snippet Cat") === false) {
+  await client.createDirectory("/Snippet Cat");
 }
 
-function addGroup() {
-  let iter = handleSnippets(1,2,3);  //  打印
-  let t1 = iter.next(); // 
-  console.log(t1); // ② Promise { <pending> }
-
- 
-  t1.then(data=>{
-    iter.next(data);iter.next(1);
-  }); // ③
-
-  
-
-
-
-  // t1.then(data=>console.log(data));
-  // iter.next().then(data=>
-  //   iter.next(data)
-  //   );
-  // // iter.next(value);
-  
-  // iter.next();
+function folderExists(p) {
+  if (fs.lstatSync(p).isDirectory()) {
+    return true;
+  }
+  return false;
 }
 
-addGroup();
+
+async function downloadDepsFiles(loaclFolderPath, syncFolderPath) {
+
+  const syncFolder = await client.getDirectoryContents(syncFolderPath);
+
+
+  syncFolder.forEach(async function (syncFileInfo) {
+    let syncFullPath = syncFileInfo.filename;
+    let syncFileName = syncFileInfo.basename;
+    let localFullPath = path.resolve(loaclFolderPath, syncFileName);
+
+    if (syncFileInfo.type !== 'directory') {
+
+      const syncStuts = await client.stat(syncFullPath);
+      const syncModTime = new Date(syncStuts.lastmod).getTime();
+
+      let localModTime = 0;
+      if(fs.existsSync(localFullPath)){
+        fs.stat(localFullPath, async (err, stat) => {
+          localModTime = stat.mtimeMs;
+        });
+      }
+
+      if (localModTime !== syncModTime) {
+        const content = await client.getFileContents(syncFullPath, { format: "text" });
+        fs.writeFileSync(localFullPath,content,{encoding: "utf8",flag: "w+",});
+      }
+
+    } else {
+
+      if (!fs.existsSync(localFullPath)) {
+        fs.mkdir(path.join(localFullPath),err=>console.log("文件不存在,已创建"));
+    }
+      downloadDepsFiles(localFullPath, syncFullPath);
+    }
+  });
+}
+
+downloadDepsFiles("H:/Snippets/Snippet Cat", "/Snippet Cat");
+
+
+
+
+
+// fs.readFile("H:\\Snippets\\Snippet Cat\\aab.txt", 'utf8', async function (err, data) {
+//   // console.log(data);
+//   await client.putFileContents("/Snippet Cat/aab.txt", "111222");
+// });
+
+// client.putFileContents("/Snippet Cat/aab.txt", "133");
+// await client.createDirectory("/Snippet Cat/php");
+
+
+// if (await client.exists("/Snippet Cat/aab.txt") === true) {
+//   const stat = await client.stat("/Snippet Cat/aab.txt");
+//   console.log(new Date(stat.lastmod).getTime());
+// }
+
+// let stuts = fs.stat("H:\\Snippets\\Snippet Cat\\aab.txt", (err, stat) => {
+//   console.log(stat.mtimeMs);
+// });
+
--- src/fileTree.ts
+++ src/fileTree.ts
@@ -2,7 +2,7 @@ import * as vscode from "vscode";
 import * as fs from "fs";
 import * as path from "path";
 import { TreeItem } from "vscode";
-import { AuthType, createClient } from "webdav";
+import { AuthType, createClient, WebDAVClient } from "webdav";
 
 /**
  * TODO:可以精剪下函数
@@ -16,10 +16,9 @@ export class TreeProvider implements vscode.TreeDataProvider<SnippetItem> {
   public treeList: SnippetItem[];
 
   private _onDidChangeTreeData: vscode.EventEmitter<SnippetItem | undefined | null | void> = new vscode.EventEmitter<
-  SnippetItem | undefined | null | void
->();
-readonly onDidChangeTreeData: vscode.Event<SnippetItem | undefined | null | void> = this._onDidChangeTreeData.event;
-
+    SnippetItem | undefined | null | void
+  >();
+  readonly onDidChangeTreeData: vscode.Event<SnippetItem | undefined | null | void> = this._onDidChangeTreeData.event;
 
   constructor(private stockRoot: string) {
     this._onDidChangeTreeData = new vscode.EventEmitter();
@@ -29,7 +28,6 @@ readonly onDidChangeTreeData: vscode.Event<SnippetItem | undefined | null | void
     this.tree;
     this._children = [];
     this._folderView = true;
-
   }
   public setTreeView(element: any) {
     this.tree = element;
@@ -132,15 +130,123 @@ readonly onDidChangeTreeData: vscode.Event<SnippetItem | undefined | null | void
     this.tree.reveal(element, { select: true, focus: true, expand: true });
   }
 
+  async uploadDepsFiles(loaclFolderPath: string, syncFolderPath: string, client: any) {
+    const localItems = fs.readdirSync(loaclFolderPath);
+    const _this = this;
+
+    localItems.forEach(async function (localFileName) {
+      let localFullPath = path.resolve(loaclFolderPath, localFileName);
+      let syncFullPath = path.join(syncFolderPath, localFileName).split(path.sep).join("/");
+
+      let isFolder = _this.folderExists(localFullPath);
+      if (!isFolder) {
+        console.log("正在上传" + localFullPath);
+        fs.stat(localFullPath, async (err, stat) => {
+          let syncModTime = 0;
+          const localModTime = stat.mtimeMs;
+
+          if ((await client.exists(syncFullPath)) === true) {
+            const syncStuts = await client.stat(syncFullPath);
+            syncModTime = new Date(syncStuts.lastmod).getTime();
+          }
+
+          if (localModTime !== syncModTime) {
+            fs.readFile(localFullPath, "utf8", async function (err, data) {
+              await client.putFileContents(syncFullPath, data, { overwrite: true });
+            });
+          }
+        });
+      } else {
+        if ((await client.exists(syncFullPath)) === false) {
+          await client.createDirectory(syncFullPath);
+        }
+        _this.uploadDepsFiles(localFullPath, syncFullPath, client);
+      }
+    });
+
+
+    const syncRes = await client.getDirectoryContents(syncFolderPath);
+    const syncItems = syncRes.map((x: any) => x.basename);
+
+    let diffItems = syncItems.filter((x: string) => !localItems.includes(x));
+
+
+    diffItems.forEach(async (x: string) => {
+      await client.deleteFile(syncFolderPath + "/" + x);
+    });
+  }
+
   async upload() {
     const client = createClient("https://drive.yuelili.com/dav", {
       username: "435826135@qq.com",
       password: "VqhY6VQNGLAg8tYitfebxrI02srnqrWr",
     });
 
-    const directoryItems = await client.getDirectoryContents("/");
+    if ((await client.exists("/Snippet Cat")) === false) {
+      await client.createDirectory("/Snippet Cat");
+    }
 
-    console.log(directoryItems);
+    let key = await vscode.window.showInputBox({ placeHolder: "确定上传吗,这将覆盖云端数据", value: "确定上传吗,这将覆盖云端数据", valueSelection: [0, 2] });
+
+    if ((await key === "确定上传吗,这将覆盖云端数据")) {
+      vscode.window.showInformationMessage("开始上传");
+      this.uploadDepsFiles(this.stockRoot, "/Snippet Cat", client);
+      vscode.window.showInformationMessage("上传完毕");
+    } else {
+      vscode.window.showInformationMessage("用户取消上传");
+    }
+
+    this.refresh();
+  }
+
+
+  async downloadDepsFiles(loaclFolderPath: string, syncFolderPath: string, client: any) {
+
+    const _this = this;
+    const syncFolder = await client.getDirectoryContents(syncFolderPath);
+    const syncItems = syncFolder.map((x: any) => x.basename);
+
+    syncFolder.forEach(async function (syncFileInfo: any) {
+      let syncFullPath = syncFileInfo.filename;
+      let syncFileName = syncFileInfo.basename;
+      let localFullPath = path.resolve(loaclFolderPath, syncFileName);
+
+      if (syncFileInfo.type !== 'directory') {
+
+        const syncStuts = await client.stat(syncFullPath);
+        const syncModTime = new Date(syncStuts.lastmod).getTime();
+
+        let localModTime = 0;
+        if (fs.existsSync(localFullPath)) {
+          fs.stat(localFullPath, async (err, stat) => {
+            localModTime = stat.mtimeMs;
+          });
+        }
+
+        if (localModTime !== syncModTime) {
+          const content = await client.getFileContents(syncFullPath, { format: "text" });
+          fs.writeFileSync(localFullPath, content, { encoding: "utf8", flag: "w+", });
+        }
+
+      } else {
+
+        if (!fs.existsSync(localFullPath)) {
+          fs.mkdir(path.join(localFullPath), err => console.log("文件不存在,已创建"));
+        }
+        _this.downloadDepsFiles(localFullPath, syncFullPath, client);
+      }
+    });
+
+    const localItems = fs.readdirSync(loaclFolderPath);
+
+    let diffItems = localItems.filter((x: string) => !syncItems.includes(x));
+
+
+    diffItems.forEach(async (x: string) => {
+      fs.rmSync(path.join(loaclFolderPath, x), { recursive: true, force: true });
+    });
+
+    this.refresh();
   }
 
   async download() {
@@ -149,22 +255,28 @@ readonly onDidChangeTreeData: vscode.Event<SnippetItem | undefined | null | void
       password: "VqhY6VQNGLAg8tYitfebxrI02srnqrWr",
     });
 
-    const directoryItems = await client.getDirectoryContents("/");
+    let key = await vscode.window.showInputBox({ placeHolder: "", value: "确定下载吗,这将覆盖本地数据", valueSelection: [0, 2] });
 
-    console.log(directoryItems);
+    if ((await key === "确定下载吗,这将覆盖本地数据")) {
+      vscode.window.showInformationMessage("开始下载");
+      this.downloadDepsFiles(this.stockRoot, "/Snippet Cat", client);
+      vscode.window.showInformationMessage("下载完毕");
+    } else {
+      vscode.window.showInformationMessage("用户取消下载");
+    }
+    
   }
 
-
-
-
-  async * handleSnippets(...args: any[]): any {
+  async *handleSnippets(...args: any[]): any {
     let [placeHolder, value, valueSelection] = args;
     let key = await vscode.window.showInputBox({ placeHolder: placeHolder, value: value, valueSelection: valueSelection });
 
     if (key !== undefined) {
       let [fun, ...args2] = yield key;
       fun(...args2, (err: any) => {
-        if (err) { vscode.window.showErrorMessage(err.toString()); }
+        if (err) {
+          vscode.window.showErrorMessage(err.toString());
+        }
       });
       this.refresh();
     } else {
@@ -179,50 +291,64 @@ readonly onDidChangeTreeData: vscode.Event<SnippetItem | undefined | null | void
   addGroup(e: SnippetItem) {
     let folderPath = e ? e.fullPath : this.stockRoot;
     let iter = this.handleSnippets("请输入文件夹名", "", [0, 0]);
-    iter.next().then((data: any) => {
-      iter.next([fs.mkdir, path.join(folderPath, data.value)]);
-    }, (err: any) => console.log(err));
+    iter.next().then(
+      (data: any) => {
+        iter.next([fs.mkdir, path.join(folderPath, data.value)]);
+      },
+      (err: any) => console.log(err)
+    );
   }
 
   editGroup(e: SnippetItem) {
     let iter = this.handleSnippets("请输入文件夹名", e.label, [0, e.label instanceof String ? e.label.length - 1 : 0]);
-    iter.next().then((data: any) => {
-      iter.next([fs.rename, e.fullPath, path.join(e.fullPath, "..", data.value)]);
-    }, (err: any) => console.log(err));
+    iter.next().then(
+      (data: any) => {
+        iter.next([fs.rename, e.fullPath, path.join(e.fullPath, "..", data.value)]);
+      },
+      (err: any) => console.log(err)
+    );
   }
 
   deleteGroup(e: any) {
     let iter = this.handleSnippets("确认删除", "确认", [0, 2]);
-    iter.next().then((data: any) => {
-      iter.next([fs.rmSync, e.fullPath, { recursive: true, force: true }]);
-    }, (err: any) => console.log(err));
+    iter.next().then(
+      (data: any) => {
+        iter.next([fs.rmSync, e.fullPath, { recursive: true, force: true }]);
+      },
+      (err: any) => console.log(err)
+    );
   }
 
-  ddSnippet(e: any) {
+  addSnippet(e: any) {
     let iter = this.handleSnippets("请输入文件名", "." + e.label, [0, 0]);
-    iter.next().then((data: any) => {
-      iter.next([fs.writeFileSync, path.join(e.fullPath, data.value), ""]);
-    }, (err: any) => console.log(err));
+    iter.next().then(
+      (data: any) => {
+        iter.next([fs.writeFileSync, path.join(e.fullPath, data.value), ""]);
+      },
+      (err: any) => console.log(err)
+    );
   }
 
   deleteSnippet(e: any) {
     let iter = this.handleSnippets("确认删除", "确认", [0, 2]);
-    iter.next().then((data: any) => {
-      iter.next([fs.rmSync, e.fullPath, { recursive: true, force: true }]);
-    }, (err: any) => console.log(err));
+    iter.next().then(
+      (data: any) => {
+        iter.next([fs.rmSync, e.fullPath, { recursive: true, force: true }]);
+      },
+      (err: any) => console.log(err)
+    );
   }
 
   editSnippet(e: any) {
     let endPos = e.icon === "file" ? e.fileName.length : e.fileName.length - e.icon.length - 1;
     let iter = this.handleSnippets("请输入新文件名", e.label, [0, endPos]);
-    iter.next().then((data: any) => {
-      iter.next([fs.rename, e.fullPath, path.join(e.fullPath, "..", data.value)]);
-    }, (err: any) => console.log(err));
+    iter.next().then(
+      (data: any) => {
+        iter.next([fs.rename, e.fullPath, path.join(e.fullPath, "..", data.value)]);
+      },
+      (err: any) => console.log(err)
+    );
   }
-
-
-
-
 }
 
 class SnippetItem extends vscode.TreeItem {
